f9c1690b967ba862044c9faad49a0af4b51a9958
Ganesh Patil <ganeshpatil1583@gmail.com>
2026-04-28 23:01:33 +0530

    Initial commit

---
+++ src/main/java/com/todo/todoapp/entity/Todo.java
@@ -0,0 +1,26 @@
+package com.todo.todoapp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="todos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false)
+    private boolean completed;
+}
