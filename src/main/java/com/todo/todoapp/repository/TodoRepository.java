0efc86c60ff635215463820b42dcd9c3e3765899
Ganesh Patil <ganeshpatil1583@gmail.com>
2026-04-28 23:07:13 +0530

    Done with repository and entity at this point

---
+++ src/main/java/com/todo/todoapp/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.todo.todoapp.repository;
+
+import com.todo.todoapp.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}
